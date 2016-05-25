a32bfdd2c4d263c5318fb3f8534c84ed83b4312a
danka74 <daniel.karlsson@liu.se>
2016-05-25 16:55:01 +0200

    added more examples

--- src/main/java/se/liu/imt/mi/snomedct/SNOMEDCTTemplatePreprocessor.java
+++ src/main/java/se/liu/imt/mi/snomedct/SNOMEDCTTemplatePreprocessor.java
@@ -54,142 +54,181 @@ public class SNOMEDCTTemplatePreprocessor {
 		set1.put("variable2", "84758475|concept3|");
 		variableLUT.add(set1);
 
-		Reader reader = new StringReader(
-				"123567|concept1|#<:{ 847857|attribute1|= [[ @variableX ]]#<, 823781|attribute2|= [[ [1..1] @variable2 ]] #>}#>"); // there is no variableX
+		String[] examples = {
+				"123567|concept1|#<:{ 847857|attribute1|= [[ @variableX ]]#<, 823781|attribute2|= [[ [1..1] @variable2 ]] #>}#>", // variableX
+																																	// not
+																																	// present
+				"123567|concept1|:#<#<{ 847857|attribute1|= [[ @variable1 ]], 823781|attribute2|= [[ @variable2 ]]#>}#>",
+				"123567|concept1|:#<{ 847857|attribute1|= [[ @variable1 ]]#<, 823781|attribute2|= [[ @variable2 ]]#>}#>",
+				"123567|concept1|:#<{ 847857|attribute1|= [[ @variable1 ]]#<, 823781|attribute2|= [[ [0..0] @variable2 ]]#>}#>", // wrong
+																																	// cardinality
+				"123567|concept1|:#<{ 847857|attribute1|= [[ @variable1 ]], 823781|attribute2|= [[ @variable2 ]]#>}#>", // missing
+																														// scope
+																														// start
+				"123567|concept1|:#<#<{ 847857|attribute1|= [[ @variable1 ]], 823781|attribute2|= @variable2 #>}#>", // missing
+																														// slot
+				"123567|concept1|:#<{ 847857|attribute1|= [[ @variable1 ]]#<, 823781|attribute2|= [[ @variableX ]]#>}#>" }; // variableX
+																															// not
+																															// present
 
-		// Reader reader = new StringReader(
-		// "123567|concept1|:#<#<{ 847857|attribute1|= [[ @variable1 ]], 823781|attribute2|= [[ @variable2 ]]#>}#>");
-		//
-		// Reader reader = new StringReader(
-		// "123567|concept1|:#<{ 847857|attribute1|= [[ @variable1 ]]#<, 823781|attribute2|= [[ @variable2 ]]#>}#>");
+		for (String example : examples) {
 
-		Writer writer = new StringWriter();
+			StringReader reader = new StringReader(example);
 
-		// create stack of StringBuilder for the different scope blocks in the
-		// template
-		Deque<StringBuilder> scopeBlockTextStack = new LinkedList<StringBuilder>();
+			System.out.print(example);
+			System.out.print(" - ");
 
-		// push a new StringBuilder for the outermost block
-		scopeBlockTextStack.push(new StringBuilder());
+			try {
+				// create stack of StringBuilder for the different scope blocks
+				// in
+				// the template
+				Deque<StringBuilder> scopeBlockTextStack = new LinkedList<StringBuilder>();
 
-		int level = 0; // keep track of level, for debugging mostly...
-		int character = reader.read();
-		while (true) {
-			if (character == -1)
-				break;
+				// push a new StringBuilder for the outermost block
+				scopeBlockTextStack.push(new StringBuilder());
 
-			switch (character) {
-			case '#':
-				// identify end of scope block '#>'
-				character = reader.read(); // look at next character
-				int prevCharacter = character;
-				if (character == '>') {
-					if (scopeBlockTextStack.size() == 1)
-						throw new Exception(
-								"Scope block end without preceding start");
-					// pop the StringBuilder corresponding to the scope that
-					// just ended
-					StringBuilder sb = scopeBlockTextStack.pop();
-					// get string including everything between but not including
-					// scope markers
-					String scopeString = sb.toString();
+				int level = 0; // keep track of level, for debugging mostly...
+				int character = reader.read();
+				while (true) {
+					if (character == -1)
+						break;
 
-					// parse the string contents of the scope
-					ANTLRInputStream is = new ANTLRInputStream(scopeString);
-					SlotLexer lexer = new SlotLexer(is);
-					CommonTokenStream tokens = new CommonTokenStream(lexer);
-					SlotParser parser = new SlotParser(tokens);
-					parser.setErrorHandler(new BailErrorStrategy());
-					// the parse tree should contain 1..* slots ([[ ... ]])
-					// possibly surrounded by arbitrary character content
-					ParseTree tree = null;
-					try {
-						tree = parser.scope();
-					} catch (ParseCancellationException pce) {
-						throw new Exception(pce);
-					}
+					switch (character) {
+					case '#':
+						// identify end of scope block '#>'
+						character = reader.read(); // look at next character
+						int prevCharacter = character;
+						if (character == '>') {
+							if (scopeBlockTextStack.size() == 1)
+								throw new Exception(
+										"Scope block end without preceding start");
+							// pop the StringBuilder corresponding to the scope
+							// that
+							// just ended
+							StringBuilder sb = scopeBlockTextStack.pop();
+							// get string including everything between but not
+							// including
+							// scope markers
+							String scopeString = sb.toString();
 
-					// get the slot corresponding to the current scope by
-					// selecting the last slot occurring within the scope
-					SlotContext slot = null;
-					if (tree.getChildCount() > 0)
-						for (int i = tree.getChildCount() - 1; i >= 0; i--) {
-							if (tree.getChild(i).getClass() == SlotContext.class) {
-								slot = (SlotContext) tree.getChild(i);
-								break;
+							// parse the string contents of the scope
+							ANTLRInputStream is = new ANTLRInputStream(
+									scopeString);
+							SlotLexer lexer = new SlotLexer(is);
+							CommonTokenStream tokens = new CommonTokenStream(
+									lexer);
+							SlotParser parser = new SlotParser(tokens);
+							parser.setErrorHandler(new BailErrorStrategy());
+							// the parse tree should contain 1..* slots ([[ ...
+							// ]])
+							// possibly surrounded by arbitrary character
+							// content
+							ParseTree tree = null;
+							try {
+								tree = parser.scope();
+							} catch (ParseCancellationException pce) {
+								throw new Exception(pce.getCause());
 							}
-						}
-					if (slot == null)
-						throw new Exception("No slot in the scope");
 
-					// extract variable name/slot path
-					VariableContext variable = slot.getChild(
-							VariableContext.class, 0);
-					String variableName = variable.getText().substring(1);
-
-					// PathContext path = slot.getChild(VariableContext.class,
-					// 0);
-
-					CardinalityContext cardinality = slot.getChild(
-							CardinalityContext.class, 0);
-					int min = 0;
-					int max = Integer.MAX_VALUE;
-					if (cardinality != null) {
-						min = Integer.parseInt(cardinality.getChild(1)
-								.getText()); // min is child no. 1
-						max = Integer.parseInt(cardinality.getChild(3)
-								.getText()); // max is child no. 3
-					}
-
-					// look up value(s) of variables/paths
-					// TODO: stub
-					String value = set1.get(variableName);
-
-					int noOfValues = 1;
-
-					// check value(s) against constraints
-					// TODO: stub
-					if (noOfValues < min || noOfValues > max)
-						throw new Exception("Cardinality constraint broken");
-
-					// for each value, output everything but the current slot
-					// which is replace by value
-					if (value != null)
-						for (int i = 0; i < noOfValues; i++) {
-							for (int j = 0; j < tree.getChildCount(); j++) {
-								if (tree.getChild(j).getClass() != SlotContext.class)
-									scopeBlockTextStack.peek().append(
-											tree.getChild(j).getText());
-								else {
-									scopeBlockTextStack.peek().append(value);
+							// get the slot corresponding to the current scope
+							// by
+							// selecting the last slot occurring within the
+							// scope
+							SlotContext slot = null;
+							int slotIndex = -1;
+							if (tree.getChildCount() > 0)
+								for (int i = tree.getChildCount() - 1; i >= 0; i--) {
+									if (tree.getChild(i).getClass() == SlotContext.class) {
+										slot = (SlotContext) tree.getChild(i);
+										slotIndex = i;
+										break;
+									}
 								}
+							if (slot == null || slotIndex == -1)
+								throw new Exception("No slot in the scope");
+
+							// extract variable name/slot path
+							VariableContext variable = slot.getChild(
+									VariableContext.class, 0);
+							String variableName = variable.getText().substring(
+									1);
+
+							// PathContext path =
+							// slot.getChild(VariableContext.class,
+							// 0);
+
+							CardinalityContext cardinality = slot.getChild(
+									CardinalityContext.class, 0);
+							int min = 0;
+							int max = Integer.MAX_VALUE;
+							if (cardinality != null) {
+								min = Integer.parseInt(cardinality.getChild(1)
+										.getText()); // min is child no. 1
+								max = Integer.parseInt(cardinality.getChild(3)
+										.getText()); // max is child no. 3
 							}
+
+							// look up value(s) of variables/paths
+							// TODO: stub
+							String value = set1.get(variableName);
+
+							int noOfValues = 1;
+
+							// check value(s) against constraints
+							// TODO: stub
+							if (noOfValues < min || noOfValues > max)
+								throw new Exception(
+										"Cardinality constraint broken: "
+												+ variableName);
+
+							// for each value, output everything but the current
+							// slot
+							// which is replace by value
+							// TODO: check when commas needs to be added
+							if (value != null)
+								for (int i = 0; i < noOfValues; i++) {
+									for (int j = 0; j < tree.getChildCount(); j++) {
+										if (j != slotIndex) // tree.getChild(j).getClass()
+															// !=
+															// SlotContext.class)
+											scopeBlockTextStack.peek().append(
+													tree.getChild(j).getText());
+										else {
+											scopeBlockTextStack.peek().append(
+													value);
+										}
+									}
+								}
+
+							level--;
+
+							break;
+
+						} else
+						// identify scope start '#<'
+						if (character == '<') {
+							// push a new Stringbuilder for the new scope block
+							level++;
+							scopeBlockTextStack.push(new StringBuilder());
+							break;
 						}
+						// write the consumed character and continue
+						scopeBlockTextStack.peek().append((char) prevCharacter);
+						continue;
+					default:
+						// append the read character to the current
+						// StringBuilder
+						scopeBlockTextStack.peek().append((char) character);
+					}
 
-					level--;
-
-					break;
-
-				} else
-				// identify scope start '#<'
-				if (character == '<') {
-					// push a new Stringbuilder for the new scope block
-					level++;
-					scopeBlockTextStack.push(new StringBuilder());
-					break;
+					character = reader.read();
 				}
-				// write the consumed character and continue
-				scopeBlockTextStack.peek().append((char) prevCharacter);
-				continue;
-			default:
-				// append the read character to the current StringBuilder
-				scopeBlockTextStack.peek().append((char) character);
+
+				System.out.println(scopeBlockTextStack.peek().toString());
+			} catch (Exception e) {
+				System.out.println(e.toString());
 			}
-
-			character = reader.read();
 		}
-
-		System.out.println(scopeBlockTextStack.peek().toString());
 	}
 
 }
